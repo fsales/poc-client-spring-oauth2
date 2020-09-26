5d105ee377b0320bb57713fecc648726606c94c6
Sales <fabio.sales@capgemini.com>
2020-09-25 21:32:10 -0300

    criação do projeto

---
+++ src/main/java/br/com/poc/clientspringoauth2/ClientSpringOauth2Application.java
@@ -0,0 +1,13 @@
+package br.com.poc.clientspringoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSpringOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientSpringOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/poc/clientspringoauth2/ClientSpringOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.clientspringoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSpringOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
